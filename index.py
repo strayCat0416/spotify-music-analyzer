7aa9e2d52ce2ae6dedda260b64eec8066b7b86f8
strayCat0416 <kyoto.masayuki@gmail.com>
2023-10-06 11:06:23 +0900

    S3アップロード処理

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import os
 import requests
 import logging
 import spotipy
+import boto3
 from spotipy.oauth2 import SpotifyClientCredentials
 from create_artist_analysis_service import CreateArtistAnalysisService
 
@@ -15,6 +16,9 @@ LINE_HEADERS = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {os.environ['LINE_ACCESS_TOKEN']}"
 }
+
+# S3関連の定数
+BUCKET_NAME = 'your-s3-bucket-name'
 
 def handler(event, context):
     try:
@@ -42,11 +46,18 @@ def handler(event, context):
                 "statusCode": 200,
                 "body": "Failed to connect with Spotify"
             }
+        
         send_line_push(user_id, f"{artist_info['name']}の楽曲情報を取得しています... しばらくお待ちください.")
         
         analysis_data = create_artist_analysis(artist_id)
-        message = create_message(analysis_data)
-        send_line_push(user_id, message)
+        
+        # アーティストの分析データをCSVとしてS3にアップロード
+        csv_content = analysis_data.to_csv(index=False, encoding='utf-8')  # pandas DataFrameをCSV文字列に変換
+        file_name = f"{artist_id}_analysis.csv"
+        csv_url = upload_to_s3(file_name, csv_content.encode('utf-8'))
+
+        # ユーザーにCSVのダウンロードリンクを送信
+        send_line_push(user_id, f"分析データはこちらのリンクからダウンロードできます: {csv_url}")
         
         return {
             "statusCode": 200,
@@ -56,26 +67,21 @@ def handler(event, context):
         send_line_push(user_id,f"不明なエラーが発生しました。{e}")
         raise(e)
 
-
 def setup_spotify_api():
     spotify_client_id = os.environ.get('SPOTIFY_CLIENT_ID')
     spotify_client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
     client_credentials_manager = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
     return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
 def create_artist_analysis(artist_id):
     service = CreateArtistAnalysisService()
     return service.create_artist_analysis_data(artist_id=artist_id)
 
-def create_message(analysis_data):
-    message = "アーティスト分析データ:\n"
-    for index, row in analysis_data.iterrows():
-        message += f"楽曲名: {row['楽曲名']}\n"
-        message += f"アーティスト名: {row['アーティスト名']}\n"
-        # ... 他のデータの表示 ...
-        message += "----------\n"
-    return message
+def upload_to_s3(file_name, file_content):
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket=BUCKET_NAME, Key=file_name, Body=file_content, ACL='public-read')
+    url = f"https://s3.amazonaws.com/{BUCKET_NAME}/{file_name}"
+    return url
 
 def send_line_push(user_id, message):
     payload = {
@@ -85,5 +91,3 @@ def send_line_push(user_id, message):
     response = requests.post(LINE_API_ENDPOINT, json=payload, headers=LINE_HEADERS)
     logger.info("Push Response status code: %s", response.status_code)
     logger.info("Push Response text: %s", response.text)
-
-
